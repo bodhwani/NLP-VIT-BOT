063a459f403aff471c2f07fec1a739330d39f01d
Vinit <vinitbodhwani123@gmail.com>
2019-03-02 19:31:17 +0530

    Revert "Added token passing between segments"

--- slang.py
+++ slang.py
@@ -5,13 +5,39 @@ from __future__ import unicode_literals
 
 from rasa_nlu.components import Component
 
-from rasa_nlu.tokenizers import Token, Tokenizer
 
 class SlangReplace(Component):
+    """A new component"""
+
+    # Name of the component to be used when integrating it in a
+    # pipeline. E.g. ``[ComponentA, ComponentB]``
+    # will be a proper pipeline definition where ``ComponentA``
+    # is the name of the first component of the pipeline.
     name = "SlangReplace"
+
+    # Defines what attributes the pipeline component will
+    # provide when called. The listed attributes
+    # should be set by the component on the message object
+    # during test and train, e.g.
+    # ```message.set("entities", [...])```
     provides = ["tokens_slangprocessed"]
+
+    # Which attributes on a message are required by this
+    # component. e.g. if requires contains "tokens", than a
+    # previous component in the pipeline needs to have "tokens"
+    # within the above described `provides` property.
     requires = ["tokens"]
+
+    # Defines the default configuration parameters of a component
+    # these values can be overwritten in the pipeline configuration
+    # of the model. The component should choose sensible defaults
+    # and should be able to create reasonable results with the defaults.
     defaults = {}
+
+    # Defines what language(s) this component can handle.
+    # This attribute is designed for instance method: `can_handle_language`.
+    # Default value is None which means it can handle all languages.
+    # This is an important feature for backwards compatibility of components.
     language_list = None
     slangs = None
     def __init__(self, component_config=None):
@@ -24,23 +50,33 @@ class SlangReplace(Component):
         pass
 
     def process(self, message, **kwargs):
+        """Process an incoming message.
+
+        This is the components chance to process an incoming
+        message. The component can rely on
+        any context attribute to be present, that gets created
+        by a call to :meth:`components.Component.pipeline_init`
+        of ANY component and
+        on any context attributes created by a call to
+        :meth:`components.Component.process`
+        of components previous to this one."""
         tokens_s=[]
-        running=0
         for token in message.get("tokens"):
-            if token.text in self.slangs:
-                tokens_s.extend([Token(tok,0) for tok in self.slangs[token.text].split(" ")])
+            if token in self.slangs:
+                tokens_s.extend(self.slangs[token].split(" "))
             else:
                 tokens_s.append(token)
-        print(tokens_s)
         message.set("tokens_slangprocessed", tokens_s)
 
     def persist(self, model_dir):
+        """Persist this component to disk for future loading."""
 
         pass
 
     @classmethod
     def load(cls, model_dir=None, model_metadata=None, cached_component=None,
              **kwargs):
+        """Load this component from file."""
 
         if cached_component:
             return cached_component
--- spell.py
+++ spell.py
@@ -6,40 +6,85 @@ from __future__ import unicode_literals
 from rasa_nlu.components import Component
 
 
-from rasa_nlu.tokenizers import Token, Tokenizer
-
-
 class SpellingCorrect(Component):
+    """A new component"""
+
+    # Name of the component to be used when integrating it in a
+    # pipeline. E.g. ``[ComponentA, ComponentB]``
+    # will be a proper pipeline definition where ``ComponentA``
+    # is the name of the first component of the pipeline.
     name = "SpellCorrection"
+
+    # Defines what attributes the pipeline component will
+    # provide when called. The listed attributes
+    # should be set by the component on the message object
+    # during test and train, e.g.
+    # ```message.set("entities", [...])```
     provides = ["token_spellchecked"]
+
+    # Which attributes on a message are required by this
+    # component. e.g. if requires contains "tokens", than a
+    # previous component in the pipeline needs to have "tokens"
+    # within the above described `provides` property.
     requires = ["tokens_slangprocessed"]
+
+    # Defines the default configuration parameters of a component
+    # these values can be overwritten in the pipeline configuration
+    # of the model. The component should choose sensible defaults
+    # and should be able to create reasonable results with the defaults.
     defaults = {}
+
+    # Defines what language(s) this component can handle.
+    # This attribute is designed for instance method: `can_handle_language`.
+    # Default value is None which means it can handle all languages.
+    # This is an important feature for backwards compatibility of components.
     language_list = None
 
     def __init__(self, component_config=None):
         super(SpellingCorrect, self).__init__(component_config)
 
     def train(self, training_data, cfg, **kwargs):
+        """Train this component.
+
+        This is the components chance to train itself provided
+        with the training data. The component can rely on
+        any context attribute to be present, that gets created
+        by a call to :meth:`components.Component.pipeline_init`
+        of ANY component and
+        on any context attributes created by a call to
+        :meth:`components.Component.train`
+        of components previous to this one."""
         print("TRAINING SPELLCHECK")
         pass
 
     def process(self, message, **kwargs):
-        from textblob import Word
+        """Process an incoming message.
+
+        This is the components chance to process an incoming
+        message. The component can rely on
+        any context attribute to be present, that gets created
+        by a call to :meth:`components.Component.pipeline_init`
+        of ANY component and
+        on any context attributes created by a call to
+        :meth:`components.Component.process`
+        of components previous to this one."""
+        import textblob as tb
         token_spellchecked=[]
         T = None
         for token in message.get("tokens_slangprocessed"):
-            print(token.text)
-            if(len(Word(token.text).spellcheck())):
-                token_spellchecked.append(Token(Word(token.text).spellcheck()[0][0],0))
-        print(token_spellchecked)
+            print(token)
+            token_spellchecked.append(token)
         message.set("token_spellchecked", token_spellchecked)
 
     def persist(self, model_dir):
+        """Persist this component to disk for future loading."""
+
         pass
 
     @classmethod
     def load(cls, model_dir=None, model_metadata=None, cached_component=None,
              **kwargs):
+        """Load this component from file."""
 
         if cached_component:
             return cached_component
