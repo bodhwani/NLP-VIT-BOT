e5cb710d053213c39ab66168369fd1925f9b20f2
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2019-03-30 17:27:13 +0530

    Add Sentimental Analysis

--- IntentTest.py
+++ IntentTest.py
@@ -1,6 +1,6 @@
 from rasa_nlu.model import Interpreter
 import json
 interpreter = Interpreter.load("./models/current/nlu")
-message = "how are placements in vit"
+message = "overrated VIT"
 result = interpreter.parse(message)
 print(json.dumps(result, indent=2))
--- quoraSearch.py
+++ quoraSearch.py
@@ -13,10 +13,14 @@ import bs4
 import requests
 import pandas as pd
 import re
+import operator
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 logger = logging.getLogger(__name__)
 
-answers = []
+sid = SentimentIntensityAnalyzer()
+
 headers = {
     
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
@@ -42,34 +46,51 @@ class QuoraSearch(Action):
 
     def QuoraSearch(self, url):
         answers = []
-        page = requests.get(url, headers=headers)
+        page = requests.get(url)
         response = page.text
         soup = bs4.BeautifulSoup(response, 'lxml')
-        container = soup.find('div',{'class':'ui_qtext_expanded'})
-        # print ("container in quora is",container)
+        # print ("soup is",soup)
+        container = soup.findAll('div',{'class':'ui_qtext_expanded'})
         for p in container:
             answers.append(p.text)
-        return answers[0]
+        # print ("answers are",answers)
+        return answers
+
+    def sentimentalAnalysis(self, quora_answer):
+        dic = {}
+        for sentence in quora_answer:
+        #     print "snetence is ",sentence[0:100],snt
+            snt = sid.polarity_scores(sentence)
+            dic[sentence] = snt['pos']
+        # for i,o in dic.iteritems():
+        #     print ("\n",i[0:50], o)
+        # print"\n\n dic is ===== ", dic
+        posAnswer = max(dic.iteritems(), key=operator.itemgetter(1))[0]
+        return posAnswer
+
 
     def run(self, dispatcher, tracker, domain):
     	query = tracker.latest_message.get('text')
-        print ("query is",query)
 
         search = query.replace(" ", "+")
         question_url = "https://www.bing.com/search?q="+search+"quora"
         # print ("question_url=",question_url)
 
         quora_link = self.GoogleSearch(question_url)
-        # print ("quora_link is ",quora_link)
+        # print ("quora link is", quora_link)
 
         quora_answer = self.QuoraSearch(quora_link)
-        if(len(quora_answer)>250):
-            quora_answer_split = quora_answer.split(".")
-            quora_answer = ".".join(quora_answer_split[:5])
-        print ("quora answers is",quora_answer)
+        # print ("Length of quora_answer is ",len(quora_answer))
+
+        sentimentalAnswer = self.sentimentalAnalysis(quora_answer)
+
+        if(len(sentimentalAnswer)>250):
+            answer_split = sentimentalAnswer.split(".")
+            sentimentalAnswer = ".".join(answer_split[:5])
+        # print ("quora answers is",quora_answer)
 
         dispatcher.utter_message("The given information is not available on VIT's website. \nBut here is what I found on Quora:\n")
-        dispatcher.utter_message(quora_answer)  # send the message back to the user
+        dispatcher.utter_message(sentimentalAnswer)  # send the message back to the user
         dispatcher.utter_message("More information can be found at:")
         dispatcher.utter_message(quora_link)
         return []
